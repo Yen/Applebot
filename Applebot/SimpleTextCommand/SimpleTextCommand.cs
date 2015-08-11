811b84724707670afa46373b40bb369021946943
TyroneSama <mash.every.key@gmail.com>
2015-08-10 23:52:08 -0700

    More groundwork for SimpleTextCommand

--- Applebot/SimpleTextCommand/SimpleTextCommand.cs
+++ Applebot/SimpleTextCommand/SimpleTextCommand.cs
@@ -13,47 +13,50 @@ namespace SimpleTextCommand
     public class SimpleTextCommand : Command
     {
         private List<Regex> _expressions = new List<Regex>();
+        private XmlNodeList _patterns;
+        private string _configLocation;
+        private XmlDocument _settings;
 
         public SimpleTextCommand()
         {
             _expressions.Add(new Regex("^!text"));
 
-            string configLocation = "SimpleTextCommand.xml";
+            _configLocation = "SimpleTextCommand.xml";
 
-            XmlDocument settings = new XmlDocument();
+            _settings = new XmlDocument();
 
-            if (!File.Exists(configLocation))
-            {
+            if (!File.Exists(_configLocation)) {
                 Logger.Log(Logger.Level.WARNING, "SimpleTextCommand config not found, one will be created");
-                XmlNode initRootNode = settings.CreateElement("patterns");
-                settings.AppendChild(initRootNode);
+                XmlNode initRootNode = _settings.CreateElement("patterns");
+                _settings.AppendChild(initRootNode);
 
-                XmlNode samplePattern = settings.CreateElement("pattern");
+                XmlNode samplePattern = _settings.CreateElement("pattern");
 
-                XmlAttribute sampleTrigger = settings.CreateAttribute("trigger");
+                XmlAttribute sampleTrigger = _settings.CreateAttribute("trigger");
                 sampleTrigger.Value = "check";
                 samplePattern.Attributes.Append(sampleTrigger);
 
-                XmlAttribute sampleResponse = settings.CreateAttribute("response");
+                XmlAttribute sampleResponse = _settings.CreateAttribute("response");
                 sampleResponse.Value = "Responding to SimpleTextCommand \"check\".";
                 samplePattern.Attributes.Append(sampleResponse);
 
                 initRootNode.AppendChild(samplePattern);
 
-                settings.Save(configLocation);
+                _settings.Save(_configLocation);
             }
 
 
             try
             {
-                settings.Load(configLocation);
+                _settings.Load(_configLocation);
             }
             catch
             {
                 Logger.Log(Logger.Level.ERROR, "Couldn't load SimpleTextCommand config file");
             }
 
-            XmlNodeList patterns = settings.SelectNodes("/patterns/pattern");
+            XmlNodeList patterns = _settings.SelectNodes("/patterns/pattern");
+            _patterns = patterns;
 
             foreach(XmlNode node in patterns)
             {
@@ -82,7 +85,32 @@ namespace SimpleTextCommand
 
         public void Execute(string user, string message, BotCore sender, BotSettings settings)
         {
-            sender.WriteChatMessage("Build test", false);
+            string[] parts = message.Split(' ');
+
+            if (parts[0] == "!text")
+            {
+                string owner = settings["channel"].ToString().Substring(1);
+                Logger.Log(Logger.Level.MESSAGE, "owner is {0}", owner);
+
+                if (user != owner)
+                {
+                    Logger.Log(Logger.Level.WARNING, "User is not owner, aborting");
+                    return;
+                }
+
+                sender.WriteChatMessage("Administrative command placeholder", false);
+                return;
+            }
+
+            foreach (XmlNode node in _patterns)
+            {
+                string trigger = node.Attributes["trigger"].Value;
+                if (trigger == message.Substring(1))
+                {
+                    sender.WriteChatMessage(node.Attributes["response"].Value, false);
+                }
+            }
+
         }
     }
 }
