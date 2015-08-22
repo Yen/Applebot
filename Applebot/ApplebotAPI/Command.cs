f9854de45dc0fcf50e82f80d549a466ff67eca48
George Bott <insertlogic@gmail.com>
2015-08-22 01:14:29 +0100

    Implemented platform class and ISender, added test command

--- Applebot/ApplebotAPI/Command.cs
+++ Applebot/ApplebotAPI/Command.cs
@@ -22,17 +22,38 @@ namespace ApplebotAPI
         public string Name { get; private set; }
         public IEnumerable<Regex> Expressions { get; private set; } = new List<Regex>();
         public TimeSpan Overflow { get; private set; }
+        public bool Droppable { get; private set; }
 
-        public static readonly TimeSpan OverflowDefault = TimeSpan.FromSeconds(5);
+        private static readonly TimeSpan OverflowDefault = TimeSpan.FromSeconds(5);
+        private static readonly bool DroppableDefault = false;
 
-        public Command(string name, TimeSpan overflow)
+        public Command(string name, TimeSpan overflow, bool droppable)
         {
             Name = name;
             Overflow = overflow;
+            Droppable = droppable;
         }
+
+        public Command(string name, TimeSpan overflow) : this(name, OverflowDefault, DroppableDefault) { }
 
         public Command(string name) : this(name, OverflowDefault) { }
 
-        public abstract void HandleMessage<T1>(T1 message) where T1 : Message;
+        public abstract void HandleMessage<T1, T2>(T1 message, T2 sender)
+            where T1 : Message
+            where T2 : ISender;
+    }
+
+    //Test
+
+    public class Ayy : Command
+    {
+        public Ayy() : base("ayy")
+        {
+        }
+
+        public override void HandleMessage<T1, T2>(T1 message, T2 sender)
+        {
+            sender.Send("123");
+        }
     }
 }
---
+++ Applebot/ApplebotAPI/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplebotAPI
+{
+    public abstract class Platform : ISender
+    {
+        public abstract void Send(string data);
+    }
+
+    public interface ISender
+    {
+        void Send(string data);
+    }
+}
