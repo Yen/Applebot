268ccefb6acfcad07e881f888a23247a8e690225
George Bott <insertlogic@gmail.com>
2015-09-29 13:38:22 +0100

    Changed short url filter to have  a 250ms delay before removing messages, this should fix errors with message orders

--- Applebot/Plugins/ShortURLFilter/ShortUrlFilter.cs
+++ Applebot/Plugins/ShortURLFilter/ShortUrlFilter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
@@ -15,7 +16,7 @@ namespace ShortURLFilter
     public class ShortURLFilter : Command
     {
 
-        string filter = @".*(bit\.ly|tinyurl\.com|clck\.ru|goo\.gl|x.co|j\.mp|ow\.ly).*";
+        string filter = @"(bit\.ly|tinyurl\.com|clck\.ru|goo\.gl|x.co|j\.mp|ow\.ly)";
 
         public ShortURLFilter() : base("ShortURLFilter", TimeSpan.Zero)
         {
@@ -25,7 +26,10 @@ namespace ShortURLFilter
         public override void HandleMessage<T1, T2>(T1 message, T2 platform)
         {
             if (!platform.CheckElevatedStatus(message))
+            {
+                Thread.Sleep(250);
                 platform.Send(new SendData(String.Format(".timeout {0} 1", message.Sender), false, message));
+            }
         }
     }
 }
