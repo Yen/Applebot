28f6ffe2ffa0c7290eaeaebfc42f6067656aea28
AJ Martinez <hallowedstream@gmail.com>
2015-09-30 02:43:56 -0700

    Blacklist: initial commit. will do more things later, right now it's basically just ShortURLFilter but i don't have to recompile the damn bot every time i see a new spam domain

---
+++ Applebot/Plugins/Blacklist/Blacklist.cs
@@ -0,0 +1,46 @@
+﻿using ApplebotAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blacklist
+{
+    [PlatformRegistrar(typeof(TwitchPlatform.TwitchPlatform))]
+    public class Blacklist : Command
+    {
+        string settingsFile = "settings/Blacklist.txt";
+
+        public Blacklist() : base("Blacklist")
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(settingsFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Expressions.Add(new Regex(line));
+                    }
+                }
+            } catch
+            {
+                Logger.Log(Logger.Level.WARNING, "Blacklist file not found, deal with this");
+                return;
+            }
+        }
+
+        public override void HandleMessage<T1, T2>(T1 message, T2 platform)
+        {
+            if (!platform.CheckElevatedStatus(message))
+            {
+                Thread.Sleep(250);
+                platform.Send(new SendData(String.Format(".timeout {0} 1", message.Sender), false, message));
+            }
+        }
+    }
+}
