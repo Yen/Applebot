45c581028bc6f77e0f7ebbd953710b594e7c1533
AJ Martinez <hallowedstream@gmail.com>
2015-10-09 10:58:54 -0700

    Blacklist: Added ability to reload filter (!blacklist reload)

--- Applebot/Plugins/Blacklist/Blacklist.cs
+++ Applebot/Plugins/Blacklist/Blacklist.cs
@@ -17,25 +17,51 @@ namespace Blacklist
 
         public Blacklist() : base("Blacklist")
         {
+            Expressions.Add(new Regex("^!blacklist\\b"));
+            bool success = Reload();
+            if (!success)
+                Logger.Log(Logger.Level.WARNING, "Blacklist couldn't be loaded! File should be in " + settingsFile);
+        }
+
+        private bool Reload()
+        {
             try
             {
                 using (StreamReader reader = new StreamReader(settingsFile))
                 {
+                    if (Expressions.Count > 1) // this is kind of an ugly place for this to be, but leaving it outside the block is ugly from a functionality standpoint, handle later?
+                        Expressions.RemoveRange(1, Expressions.Count - 1);
                     string line;
                     while ((line = reader.ReadLine()) != null)
                     {
                         Expressions.Add(new Regex(line));
                     }
                 }
-            } catch
+                return true;
+            }
+            catch
             {
-                Logger.Log(Logger.Level.WARNING, "Blacklist settings not found! (Should be in settings/Blacklist.txt)");
-                return;
+                return false;
             }
         }
 
+
         public override void HandleMessage<T1, T2>(T1 message, T2 platform)
         {
+            string[] parts = message.Content.Split(' ');
+            if (parts[0] == "!blacklist" && parts.Length > 1 && platform.CheckElevatedStatus(message))
+            {
+                switch (parts[1])
+                {
+                    case "reload":
+                        bool success = Reload();
+                        platform.Send(new SendData(success ? "Blacklist was reloaded successfully." : "Blacklist couldn't be loaded. Check file path?", false, message));
+                        break;
+                }
+            }
+
+
+
             if (!platform.CheckElevatedStatus(message))
             {
                 Thread.Sleep(250);
