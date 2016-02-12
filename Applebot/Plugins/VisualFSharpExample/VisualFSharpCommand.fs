35aa1baa0df0e7380b91c25e624a52d9a24e1c62
George Bott <insertlogic@gmail.com>
2016-02-12 01:36:29 +0000

    Removed example projects, they were outdated anyway. Changed all projects to use .NET 4.5 or lower to allow for mono compilation

--- Applebot/Plugins/VisualFSharpExample/VisualFSharpCommand.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace VisualFSharpExample
-
-open ApplebotAPI
-
-[<PlatformRegistrar(typedefof<Platform>)>]
-type VisualFSharpCommand() = 
-    inherit Command("Visual FSharp Command")
-    override this.HandleMessage<'T1, 'T2 when 'T1 :> Message and 'T2 :> Platform>(message : 'T1, platform : 'T2) = 
-        Logger.Log("Response from Visual FSharp Command")
