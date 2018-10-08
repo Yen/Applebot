66da512af17be3934f3a1557d6b7ae70c4500f3f
uulltt <ajvark@gmail.com>
2018-10-07 21:58:44 -0400

    adding haiku feature. sorry if i break anything.

--- src/messageHandlers/markov.ts
+++ src/messageHandlers/markov.ts
@@ -5,6 +5,7 @@ import MessageFloodgate from "../messageFloodgate";
 import * as sqlite from 'sqlite';
 import * as Discord from "discord.js";
 import DiscordExtendedInfo from "../extendedInfos/discordExtendedInfo";
+var syllable = require('syllable');
 
 class Markov implements MessageHandler {
 
@@ -15,8 +16,12 @@ class Markov implements MessageHandler {
 	private _getNext: sqlite.Statement;
 	private _getWord: sqlite.Statement;
 
-	private _wordArray: { [id: number]: string };
-	private _idArray: { [word: string]: number };
+	private _wordArray: {
+		[id: number]: string
+	};
+	private _idArray: {
+		[word: string]: number
+	};
 
 	private _emotes: string[] = [":o", ":0", ":v", ":u", ":?", ":o", ":I"];
 	private _names: string[] = ["applebot", "appleb0t", "<@213048998678888448>"];
@@ -42,8 +47,13 @@ class Markov implements MessageHandler {
 		return markov;
 	}
 
-	async handleMessage(responder: (content: string) => Promise<void>, content: string, info: ExtendedInfo | undefined) {
+	async handleMessage(responder: (content: string) => Promise < void > , content: string, info: ExtendedInfo | undefined) {
 		content = content.toLowerCase();
+		var haiku = false;
+		if (/^!haiku/gm)
+			.test(content) {
+				haiku = true;
+			}
 		let args = content.split(" ");
 
 		//behavior
@@ -84,7 +94,7 @@ class Markov implements MessageHandler {
 		if (train) {
 			await Promise.all(args
 				.filter(w => this._idArray[w] === undefined)
-				.map(async (w) => {
+				.map(async(w) => {
 					await this._trainWords.run([w]);
 					const insertedWord = await this._getWord.get(w);
 					this._wordArray[insertedWord.id] = w;
@@ -106,7 +116,7 @@ class Markov implements MessageHandler {
 		}
 
 		//early out if message doesn't need to be generated
-		if (!forceResponse && Math.random() > 0.01) {
+		if (!forceResponse && !haiku && Math.random() > 0.01) {
 			return;
 		}
 
@@ -126,10 +136,50 @@ class Markov implements MessageHandler {
 				if (sequence[sequence.length - 1] == null)
 					sequence.pop();
 				let message = [];
+				let line0 = [];
+				let line1 = [];
+				let line2 = [];
+				let linelen = [0, 0, 0];
 				for (let id of sequence) {
-					message.push(this._wordArray[id]);
+					if (haiku) {
+						if (linelen[0] < 5) {
+							linelen[0] += syllable(this._wordArray[id]);
+							if (linelen[0] > 5) {
+								linelen[0] -= syllable(this._wordArray[id]);
+								continue;
+							} else {
+								line0.push(this._wordArray[id]);
+							}
+						} else if (linelen[1] < 7) {
+							linelen[1] += syllable(this._wordArray[id]);
+							if (linelen[1] > 7) {
+								linelen[1] -= syllable(this._wordArray[id]);
+								continue;
+							} else {
+								line1.push(this._wordArray[id]);
+							}
+
+						} else if (linelen[2] < 5) {
+							linelen[2] += syllable(this._wordArray[id]);
+							if (linelen[2] > 5) {
+								linelen[2] -= syllable(this._wordArray[id]);
+								continue;
+							} else {
+								line2.push(this._wordArray[id]);
+							}
+						} else {
+							break;
+						}
+					} else {
+						message.push(this._wordArray[id]);
+					}
 				}
+				if (!haiku){
 				candidates.push(message.join(" "));
+				} else {
+					message = [line0.join(" "), line1.join(" "), line2.join(" ")];
+					candidates.push(message.join("\n"));
+				}
 			}
 		}
 
@@ -146,9 +196,10 @@ class Markov implements MessageHandler {
 
 		const distances = candidates.map(c => Math.abs(this._targetLength - (c.split(" ").length - 1)));
 		const maxDistance = distances.reduce(function (a, b) {
-			return Math.max(a, b);
-		});
+				return Math.max(a, b);
+			});
 		const weightedCandidates = []
+
 		for (let i in candidates) {
 			for (let j = 0; j <= maxDistance - distances[i]; j++) {
 				weightedCandidates.push(candidates[i]);
@@ -156,14 +207,24 @@ class Markov implements MessageHandler {
 		}
 
 		let response = weightedCandidates[Math.floor(Math.random() * weightedCandidates.length)]; // this might change LOL
+		if (haiku) {
+			var choices = weightedCandidates.map(function (item) {
+					return (item.match(/[a-zA-Z]+/gm).map(function (item) {
+							return syllable(item);
+						})).reduce((a, b) => a + b, 0);
+				});
+			if (choices.indexOf(17) != -1) {
+				response = weightedCandidates[choices.indexOf(17)];
+			}
 
+		}
 		//cleanup
 		response = response.replace(/,$/, "");
 		if (/[!?\.]$/.test(response) == false)
 			response = response + ".";
 		response = response + " " + this._emotes[Math.floor(Math.random() * this._emotes.length)]
-		await responder(response);
+			await responder(response);
 	}
 }
 
-export default Markov;
+export defaultMarkov;
